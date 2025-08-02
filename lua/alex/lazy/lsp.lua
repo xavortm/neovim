f785d7aaba3a94bb441bb9af979c16d5aeaf8aa4
Alexander Dimitrov <xavortm@gmail.com>
2025-08-02 10:24:39 +0300

    add inline hints and more

--- lua/alex/lazy/lsp.lua
+++ lua/alex/lazy/lsp.lua
@@ -17,55 +17,7 @@ return {
       intelephense = {
         settings = {
           intelephense = {
-            stubs = {
-              "bcmath",
-              "bz2",
-              "calendar",
-              "Core",
-              "ctype",
-              "curl",
-              "date",
-              "dom",
-              "exif",
-              "fileinfo",
-              "filter",
-              "ftp",
-              "gd",
-              "gettext",
-              "hash",
-              "iconv",
-              "imagick",
-              "intl",
-              "json",
-              "libxml",
-              "mbstring",
-              "mysqli",
-              "mysqlnd",
-              "openssl",
-              "password",
-              "pcre",
-              "PDO",
-              "pdo_mysql",
-              "Phar",
-              "readline",
-              "redis",
-              "Reflection",
-              "session",
-              "SimpleXML",
-              "soap",
-              "sockets",
-              "sodium",
-              "SPL",
-              "standard",
-              "tokenizer",
-              "xml",
-              "xmlreader",
-              "xmlwriter",
-              "zip",
-              "zlib",
-              "wordpress",
-              "php"
-            },
+            stubs = { "bcmath", "bz2", "calendar", "Core", "ctype", "curl", "date", "dom", "exif", "fileinfo", "filter", "ftp", "gd", "gettext", "hash", "iconv", "imagick", "intl", "json", "libxml", "mbstring", "mysqli", "mysqlnd", "openssl", "password", "pcre", "PDO", "pdo_mysql", "Phar", "readline", "redis", "Reflection", "session", "SimpleXML", "soap", "sockets", "sodium", "SPL", "standard", "tokenizer", "xml", "xmlreader", "xmlwriter", "zip", "zlib", "wordpress", "php" },
             files = {
               maxSize = 1000000
             },
@@ -109,8 +61,30 @@ return {
   },
   config = function(_, opts)
     local lspconfig = require('lspconfig')
+
+    -- Defines the on_attach function
+    local on_attach = function(client, bufnr)
+      -- lsp_signature
+      require("lsp_signature").on_attach({
+        bind = true,
+        handler_opts = {
+          border = "rounded",
+        },
+      }, bufnr)
+    end
+
     for server, config in pairs(opts.servers) do
       config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+      --
+      -- Attaches the on_attach function to each server
+      -- while preserving any existing on_attach functions
+      local original_on_attach = config.on_attach
+      config.on_attach = function(client, bufnr)
+        if original_on_attach then
+          original_on_attach(client, bufnr)
+        end
+        on_attach(client, bufnr)
+      end
       lspconfig[server].setup(config)
     end
   end
