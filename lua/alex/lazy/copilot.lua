0712cb51742fcf4c0e35d001feed000244722f09
alex.dimitrov <xavortm@gmail.com>
2025-08-08 11:24:46 +0300

    tweaks to copilot

--- lua/alex/lazy/copilot.lua
+++ lua/alex/lazy/copilot.lua
@@ -1,3 +1,14 @@
 return {
-  "github/copilot.vim",
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-l>", 'copilot#Accept("<CR>")', {
+        expr = true,
+        silent = true,
+        noremap = true
+      })
+    end,
+  }
 }
