4f49a1bcadc0329083854d27a5d7e67e03fb2f29
alex.dimitrov <xavortm@gmail.com>
2025-08-11 11:47:50 +0300

    new color scheme

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
 -- Load the main configuration for Alex's Neovim setup
 require("alex")
+
+-- Load the color scheme...
+vim.cmd [[colorscheme moonfly]]
+vim.g.moonflyNormalFloat = true
+vim.g.moonflyTransparent = true
